ae92c84469fb47466c6af266ac0d0200747721d2
anton.shcherbyna <anton.shcherbyna@sigma.software>
2018-03-17 17:21:15 +0200

    Create validator

--- index.js
+++ index.js
@@ -1,22 +1,77 @@
-exports.printMsg = function() {
-  console.log("This is a message from the demo package");
-}
+const os = require('os')
+const executeCommand = require('child_process').execSync;
+const readFile = require('fs').readFileSync;
 
-const childProcess = require('child_process');
-const fs = require('fs');
-console.log("=========commit-msg==========");
-console.log(process.env.GIT_PARAMS);
-const commitMessage = fs.readFileSync(process.env.GIT_PARAMS, "utf8");
-if (commitMessage.match('(feature|bugfix|merge)\/(CSDP-)')) {
-    console.log("success");
-} else {
-    console.log("failed");
-    throw new Error();
-}
-console.log(commitMessage);
+validateGitArtifacts(process.argv.slice(2));
 
-console.log("===========commit-msg============");
+function validateGitArtifacts(args) {
+    const options = parseOptions(args);
 
-function getCurrentBranch() {
-    return childProcess.execFileSync('git', ['rev-parse', '--abbrev-ref', 'HEAD']).toString();
+    const targets = {
+        branch: validateBranch,
+        commitmsg: validateCommitmsg
+    };
+
+    validate(targets, options);
+
+    function parseOptions(args) {
+        if (args.length < 2) {
+            throw error(`You pass less arguments then expected.${getHelpMessage()}`);
+        }
+
+        if (args.length > 2) {
+            throw error(`You pass more arguments then expected. ${getHelpMessage()}`);
+        }
+
+        return {
+            target: args[0],
+            pattern: args[1]
+        };
+
+        function getHelpMessage() {
+            const currentFilename = __filename.split(/[\\/]/).pop();
+            return `${os.EOL} Please use command: ${os.EOL}`
+                + `$ node ${currentFilename} [validation target (branch,commitmsg)] [Regex pattern] ${os.EOL}`
+                + `$ node ${currentFilename} commitmsg \'issue-\'`;
+        }
+    }
+
+    function validate(targets, options) {
+        const targetsKeys = Object.keys(targets);
+        if (!targetsKeys.includes(options.target)) {
+            error(`Target argument: '${options.target}' is not support. Please use following: ${targetsKeys}.`);
+        }
+
+        targets[options.target](options.pattern);
+    }
+
+    function validateCommitmsg(pattern) {
+        const commitMessage = readFile(process.env.GIT_PARAMS, "utf8");
+        if (!commitMessage.match(pattern)) {
+            error(`Commit message '${commitMessage}' is not allowed by pattern '${pattern}'.`);
+        }
+    }
+
+    function validateBranch(pattern) {
+        const branchName = getCurrentBranchName();
+
+        if (!branchName.match(pattern)) {
+            error(`Branch name '${branchName}' is not allowed by pattern '${pattern}'.`);
+        }
+
+        function getCurrentBranchName() {
+            const branchName = executeCommand('git rev-parse --abbrev-ref HEAD');
+
+            if (!branchName) {
+                error('Unable to determine branch name using git command.');
+            }
+
+            return branchName.toString();
+        }
+    }
+
+    function error(message) {
+        console.log(message);
+        process.exit(1);
+    }
 }

214ae35958c0bb497c57eebe58d312d181350998
khdevnet <khdevnet@gmail.com>
2018-03-18 00:52:23 +0200

    Move commitmsg to cli folder

--- cli/commitmsg.js
+++ cli/commitmsg.js
@@ -2,6 +2,6 @@ const argv = require('minimist')(process.argv.slice(2));
 const readFile = require('fs').readFileSync;
 
 const commitMessageValidator = require('../src/validators/commit-message-validator');
-console.log(argv);
+
 const commitMessage = readFile(argv.g, 'utf8')
 commitMessageValidator(commitMessage, argv.p);

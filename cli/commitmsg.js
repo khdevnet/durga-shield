383f4627619fdeaaf21e53e103186c6974203b45
khdevnet <khdevnet@gmail.com>
2018-03-18 14:24:45 +0200

    test

--- cli/commitmsg.js
+++ cli/commitmsg.js
@@ -6,4 +6,5 @@ const argv = minimist(process.argv.slice(2))
 const gitCommitMessageFilePath = argv.g;
 const validationPattern = argv.p;
 console.log(argv);
+console.log('test test test======');
 commitMessageValidator(gitCommitMessageFilePath, validationPattern);

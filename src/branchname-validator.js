d1f46309cc825187218fbc920a82a51fdabced81
khdevnet <khdevnet@gmail.com>
2018-03-18 16:55:49 +0200

    issue-1 Add readme

--- src/branchname-validator.js
+++ src/branchname-validator.js
@@ -5,10 +5,8 @@ const getConf = require('./config-loader');
 
 
 module.exports = function () {
-  const pattern = getConf(process.cwd()).branchName;
-  console.log(pattern);
+  const pattern = getConf(process.cwd()).branchname;
   const branchName = getCurrentBranchName();
-  console.log(branchName);
 
   if (!branchName.match(pattern)) {
     throw new Error(`${os.EOL} Branch name is not allowed by pattern ${pattern}.${os.EOL} Branch name: ${branchName}`);
